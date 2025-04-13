27e16ceeb60576b16987df0e471e9e338ff10400
SvetlanaPostnikova <postn1kovasv05@gmail.com>
2025-04-13 19:58:35 +0400

    Replaced fixed-angle rotation with arbitrary angle rotation

--- src/modules/image_edit.h
+++ src/modules/image_edit.h
@@ -6,6 +6,6 @@ extern const fs::path result_folder;
 void transform_image_crop(std::string filepath, image_codec* codec, 
                          unsigned int crop_left, unsigned int crop_top,
                          unsigned int crop_right, unsigned int crop_bottom);
-void transform_image_rotate(std::string filepath, image_codec* codec, unsigned angle);
 void transform_image_reflect(std::string filepath, image_codec* codec, bool horizontal, bool vertical);
 void transform_image_shear(std::string filepath, image_codec* codec, float shx, float shy);
+void transform_image_rotate(std::string filepath, image_codec* codec, float angle);
--- src/modules/image_transforms.h
+++ src/modules/image_transforms.h
@@ -1,6 +1,6 @@
 #include "image_tools.h"
 void crop(matrix& img, unsigned crop_left, unsigned crop_top, unsigned crop_right, unsigned crop_bottom);
-void rotate(matrix& img, unsigned angle);
 void reflect(matrix& img, bool horizontal, bool vertical);
 void shear(matrix& img, float shx, float shy);
 void bilinear_interpolate(matrix& img, float x, float y, unsigned char* result);
+void rotate(matrix& img, float angle);
