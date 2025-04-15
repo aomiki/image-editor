908b95da1325f0e74f0e56460b8334c2239769fe
aomiki <aomiki.milky@icloud.com>
2025-04-15 10:59:16 +0400

    rotate on cuda

--- src/modules/impls_hw_accel/cuda/image_transforms.cu
+++ src/modules/impls_hw_accel/cuda/image_transforms.cu
@@ -12,11 +12,42 @@ __global__ void kernel_crop_memcpy(matrix* img_src, matrix* img_dest, uint2 src_
         return;
     }
 
-    unsigned char* curr_pixel_src = img_src->get(x + src_offset.x, y + src_offset.y);
-    unsigned char* curr_pixel_dest = img_dest->get(x, y);
+    unsigned char* old_pixel = img_src->get(x + src_offset.x, y + src_offset.y);
+    unsigned char* new_pixel = img_dest->get(x, y);
     for (size_t i = 0; i < img_src->components_num; i++)
     {
-        curr_pixel_dest[i] = curr_pixel_src[i];
+        new_pixel[i] = old_pixel[i];
+    }
+}
+
+__global__ void kernel_rotate_memcpy(matrix* img_src, matrix* img_dest, unsigned angle)
+{
+    unsigned int x = threadIdx.x + blockDim.x * blockIdx.x;
+    unsigned int y = threadIdx.y + blockDim.y * blockIdx.y;
+
+    if (x >= img_src->width || y >= img_src->height)
+    {
+        return;
+    }
+
+    unsigned char* old_pixel = img_src->get(x, y); 
+    unsigned char* new_pixel = nullptr;  
+    
+    switch (angle) {
+        case 90:
+            new_pixel = img_dest->get(img_dest->width - y - 1, x);
+            break;
+        case 180:
+            new_pixel = img_dest->get(img_dest->width - x - 1, img_dest->height - y -1);
+            break;
+        case 270:
+            new_pixel = img_dest->get(y, img_dest->height - x - 1);
+            break;
+    }
+
+    for (size_t i = 0; i < img_src->components_num; i++)
+    {
+        new_pixel[i] = old_pixel[i];
     }
 }
 
@@ -87,6 +118,57 @@ void crop(matrix& img, unsigned crop_left, unsigned crop_top, unsigned crop_righ
 
 void rotate(matrix& img, unsigned angle) 
 {
-    
-}
+    angle = angle % 360;  
+    if (angle == 0) return; 
 
+    unsigned new_width = (angle == 90 || angle == 270) ? img.height : img.width;
+    unsigned new_height = (angle == 90 || angle == 270) ? img.width : img.height;
+
+    unsigned total_blocksize = 32;
+    if (img.size() >= 4480)
+    {
+        total_blocksize = 128;
+    }
+
+    if (img.size() >= 8960)
+    {
+        total_blocksize = 256;
+    }
+
+    if (img.size() >= 17920)
+    {
+        total_blocksize = 512;
+    }
+
+    if (img.size() >= 35840)
+    {
+        total_blocksize = 1024;
+    }
+
+    int blocksize_2d = (int)(total_blocksize/img.components_num);
+    int blocksize_1d = (int)sqrt(blocksize_2d);
+
+    int blocksnum_x = (int)(img.width / blocksize_1d + 1);
+    int blocksnum_y = (int)(img.height / blocksize_1d + 1);
+
+    dim3 blockSize(blocksize_1d, blocksize_1d, img.components_num);
+    dim3 gridSize(blocksnum_x, blocksnum_y);
+
+    matrix* d_img;
+    cudaMalloc(&d_img, sizeof(matrix));
+    cudaMemcpy(d_img, &img, sizeof(matrix), cudaMemcpyHostToDevice);
+
+    unsigned char* d_rotated_arr;
+    cudaMalloc(&d_rotated_arr, sizeof(unsigned char) * img.size_interlaced());
+    img.set_arr_interlaced(d_rotated_arr, new_width, new_height);
+
+    matrix* d_img_rotated;
+    cudaMalloc(&d_img_rotated, sizeof(matrix));
+    cudaMemcpy(d_img_rotated, &img, sizeof(matrix), cudaMemcpyHostToDevice);
+
+    kernel_rotate_memcpy<<<gridSize, blockSize>>>(d_img, d_img_rotated, angle);
+
+    cudaMemcpy(&img, d_img_rotated, sizeof(matrix), cudaMemcpyDeviceToHost);
+    cudaFree(d_img);
+    cudaFree(d_img_rotated);
+}
